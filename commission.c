d0c3bae5623ff7d339c83081ea32a8b64deeceed
E-rick23 <67922086+E-rick23@users.noreply.github.com>
2022-03-29 19:48:00 -0300

    Add files via upload

---
+++ commission.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+/*This code calculates the total salary of an employee,
+  considering a commission of 15% on their sellings. */
+int main() {
+   //Declaring variables
+   char name;
+   double sellings, salary, commission, everything;
+    printf("Type the name: ");
+    //Receiving the employee name.
+   scanf("%s", &name);
+    printf("Type the salary: ");
+    //Receiving the employee salary.
+   scanf("%lf", &salary);
+    printf("Type how much the employee sold: ");
+    //Receiving the employee sellings.
+   scanf("%lf", &sellings);
+    //Calculating the commission.
+   commission = sellings*(0.15);
+    //Adding the commission to the salary.
+   everything = salary+commission;
+    //Printing the final salary.
+   printf("TOTAL = R$ %.2lf", everything);
+    return 0;
+}
