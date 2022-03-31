e87e29ee1b023c3420dd9a70e2bb2b258bb680b0
E-rick23 <67922086+E-rick23@users.noreply.github.com>
2022-03-31 20:29:15 -0300

    Changed from "addition.c" to "basic operations.c"
    Decided to work with all basic operations on this code, might be more useful.

---
+++ C/basic operations.c
@@ -0,0 +1,28 @@
+//This code calculates the basic operations of two numbers.
+#include <stdio.h>
+
+int main()
+{
+     //Declaring variables.
+     double a, b, addition, subtraction, multiplication, division;
+     //Asking for the input of two values.
+     printf("Please enter the first number: ");
+     scanf("%lf", &a);
+     printf("Please enter the second number: ");
+     scanf("%lf", &b);
+     //Addition operation.
+     addition = a+b;
+     //Subtraction operation.
+     subtraction = a-b;
+     //Division operation.
+     division = a/b;
+     //Multiplication operation.
+     multiplication = a*b;
+     //Printing the results
+     //The values will be rounded up the final value to two digits after the "."
+     printf("a+b = %.2lf\n", addition);
+     printf("a-b = %.2lf\n", subtraction);
+     printf("a/b = %.2lf\n", division);
+     printf("a*b = %.2lf\n", multiplication);
+    return 0;
+}
