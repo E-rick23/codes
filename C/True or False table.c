984045375bd3542bdf9c334dc50bd5c097ebda94
E-rick23 <67922086+E-rick23@users.noreply.github.com>
2022-04-01 11:30:16 -0300

    Updated commentaries and line breaks

--- C/True or False table.c
+++ C/True or False table.c
@@ -5,10 +5,11 @@ int main()
 {
     char a, symbol, b;
 	printf("Welcome, this program allows you to see the truth tables for the operators &, | and ^.\n");
-	printf("This program uses the following format: x symbol y. \n").
+	printf("This program uses the following format: x symbol y. \n");
 	printf("Please input the table you want to know about: ");
     scanf("%c %c %c", &a, &symbol, &b);
-
+	//After the input is made, the program will answer with the respective truth table.
+	printf("This is the truth table for %c %c %c:\n", a, symbol, b);
     if (symbol == '&'){
     		printf("-------\n");
     		printf("|%c||%c||r|\n", a, b);
