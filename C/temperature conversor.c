46054bd5a6cfc0b2afc062809dbf697bd1df8f22
E-rick23 <erickmarques63@gmail.com>
2022-03-31 21:42:27 -0300

    Basic temperature converter (still working on it)

---
+++ C/temperature conversor.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+    float temperature;
+    char base, symbol, convert;
+    
+    scanf("%f %c %c %c", &temperature, &base, &symbol, &convert);
+        if (base == "C" || base == "c"){
+            if (convert == "F" || convert == "f"){
+                
+            }
+        }
+    printf("%f %c %c %c", temperature, base, symbol, convert);
+    return 0;
+}
