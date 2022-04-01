fca05ffd10f3754e25fd9c4d9793c562e4cb164e
E-rick23 <67922086+E-rick23@users.noreply.github.com>
2022-03-31 22:36:13 -0300

    temperature conversor update

--- C/temperature conversor.c
+++ C/temperature conversor.c
@@ -3,15 +3,41 @@
 int main()
 {
 
-    float temperature;
-    char base, symbol, convert;
+    float temperature, f, k;
+    char base, convert;
     
-    scanf("%f %c %c %c", &temperature, &base, &symbol, &convert);
-        if (base == "C" || base == "c"){
-            if (convert == "F" || convert == "f"){
-                
-            }
-        }
-    printf("%f %c %c %c", temperature, base, symbol, convert);
+    scanf("%f %c > %c", &temperature, &base, &convert);
+        if (base == 'C' || base == 'c'){
+            if (convert == 'F' || convert == 'f'){
+                f = temperature*1.8+32;
+                printf("%.2f F", f);
+            };
+            if (convert == 'K' || convert == 'k'){
+                k = temperature+273.15;
+                printf("%.2f K", k);
+            };
+        };
+        if (base == 'F' || base == 'f'){
+            if (convert == 'C' || convert == 'c'){
+                f = (temperature-32)/1.8;
+                printf("%.2f C", f);
+            };
+            if (convert == 'K' || convert == 'k'){
+                f = (temperature-32)/1.8;
+                k = f+273.15;
+                printf("%.2f K", k);
+            };
+        };
+        if (base == 'K' || base == 'k'){
+            if (convert == 'C' || convert == 'c'){
+                k = temperature - 273.15;
+                printf("%.2f C", k);
+            };
+            if (convert == 'F' || convert == 'f'){
+                k = temperature - 273.15;
+                f = k * 1.8 + 32;
+                printf("%.2f F", f);
+            };
+        };
     return 0;
 }
