b6a2a25a61470a09a8cb74b082bcff837977e3a1
E-rick23 <erickmarques63@gmail.com>
2022-04-02 22:59:43 -0300

    Updated commentaries

--- JavaScript/average.js
+++ JavaScript/average.js
@@ -1,14 +1,20 @@
-var values, counter, x, total, average;
-values = parseInt(prompt("Type how many numbers you'll work with: "));
-counter = 0;
-total = 0;
-average = 0;
-x = 0;
+//This code calculates the average of the amount of numbers the user needs.
 
+//Declaring variables...
+var values, counter, x, total, average;
+//Setting variables default value...
+counter = total = average = x = 0;
+//First, the program asks for input, and converts the input to a int number.
+values = parseInt(prompt("Type how many numbers you'll work with: "));
+//After that, this while loop keeps counting until the number of values the user wants to work with is reached.
 while (counter < values){
     x = parseFloat(prompt(`Type the ${counter+1}º value: `));
+    //The value given in x is added to the total variable.
     total = total+x;
+    //The counter goes up by one, each time a value is entered.
     counter = counter+1;
 };
+//In the end, the average variable divides the sum of the values entered by the amount of values.
 average = total/values;
+//After that, it notifies the user the average of the values.
 alert("The average is:" + average);
