77d5993d7a89255145bec165ed07389ac23071ef
E-rick23 <erickmarques63@gmail.com>
2022-04-02 21:32:40 -0300

    Added commentaries

--- JavaScript/Hours to days.js
+++ JavaScript/Hours to days.js
@@ -6,8 +6,12 @@ var hours, hour, days, daysandhours;
 
 hours = prompt();
 hour = parseFloat(hours);
+//Considering 24 hours make a day, the variable days makes a division that assures every 24 hours becomes 1 day.
 days = parseInt(hour/24); 
+//The variable daysandhours gets what's left of the division.
 daysandhours = parseInt(hour%24);
+/*If daysandhours in fact has a value, then, the program will show the user the days, and hours (which are the rest of the division).
+  Or else, if the division rest is 0, it will just return the number of days.*/
 if (daysandhours > 0){
     alert(`${hours} hours in days are: ${days} days ${daysandhours} hours!`);
 } else {
