b004128005d4138630b52e125ab0c293076bb55f
E-rick23 <erickmarques63@gmail.com>
2022-04-02 22:51:15 -0300

    updated average.js

--- JavaScript/average.js
+++ JavaScript/average.js
@@ -1,9 +1,12 @@
 var values, counter, x, total, average;
 values = parseInt(prompt("Type how many numbers you'll work with: "));
 counter = 0;
+total = 0;
+average = 0;
+x = 0;
 
 while (counter < values){
-    x = prompt(`Type the ${c+1}º value: `);
+    x = parseFloat(prompt(`Type the ${counter+1}º value: `));
     total = total+x;
     counter = counter+1;
 };
