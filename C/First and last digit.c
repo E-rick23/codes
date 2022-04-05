692a455a3ab120741759ca1e0e93c01e665f47a1
E-rick23 <erickmarques63@gmail.com>
2022-04-05 16:27:30 -0300

    Updated First and last digit.c

--- C/First and last digit.c
+++ C/First and last digit.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
 int main(){
+    int number, first, last;
+    number = first = last = 0;
+    scanf("%d", &number);
+    last = number % 10;
+    while ( number >= 10 ){
+        number = number/10;
+    }
+    first = number;
+    printf("O ultimo número é: %d\n", last);
+    printf("O primeiro número é: %d\n", first);
+    number = first+last;
+    printf("A soma é: %d\n", number);
     return 0;
 }
