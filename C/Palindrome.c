028f390b823d1ff2836d21cd0892f21e7eb4c842
E-rick23 <erickmarques63@gmail.com>
2022-04-05 15:57:42 -0300

    Added palindrome.c

---
+++ C/Palindrome.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    int x, reverse, restm, copyx;
+    x = reverse = restm = 0;  
+    scanf("%d", &x);    
+    copyx = x;
+    while(x!=0)    
+    {    
+     restm=x%10;    
+     reverse=reverse*10+restm;    
+     x/=10;    
+    }    
+    if (copyx == reverse){
+        printf("%d is a palindrome ", copyx);
+        if ((copyx%2) == 0){
+            printf("and an even number.");
+        }
+        else
+        {
+            printf("and an odd number.");
+        };
+    }
+    else 
+    {
+        printf("%d isn't a palindrome.", copyx);
+    };
+    return 0;
+};
