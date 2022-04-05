807f7372c45b8deaeaca4f37de6e7c1e6b79b529
E-rick23 <erickmarques63@gmail.com>
2022-04-04 21:51:54 -0300

    Added underweight categories

--- JavaScript/body mass index.js
+++ JavaScript/body mass index.js
@@ -1,7 +1,16 @@
-var p = prompt("Enter your weight");
-var a = prompt("Enter your height");
-var imc = p/(a*a);
-if (imc < 25) {
+var weight = prompt("Enter your weight");
+var height = prompt("Enter your height");
+var bmi = weight/(height*height);
+if (bmi < 16){
+    alert(`Your BMI is: ${bmi}.\nIt is considered as: Underweight (Severe Thinness).`);
+}
+if (bmi >= 16 && bmi <= 16.9){
+    alert(`Your BMI is: ${bmi}.\nIt is considered as: Underweight (Moderate thinness).`);
+}
+if (bmi >= 17 && bmi <= 18.4){
+    alert(`Your BMI is: ${bmi}.\nIt is considered as: Underweight (Mild thinness).`);
+}
+if (bmi < 25) {
     console.log("Normal");
 } else {
     console.log("Overweight");
