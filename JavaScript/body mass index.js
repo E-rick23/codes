fdb3d5779a5ffc4d41f1c699ed04dca2aa630913
E-rick23 <erickmarques63@gmail.com>
2022-04-04 21:55:51 -0300

    Added normal and overweight categories

--- JavaScript/body mass index.js
+++ JavaScript/body mass index.js
@@ -3,15 +3,16 @@ var height = prompt("Enter your height");
 var bmi = weight/(height*height);
 if (bmi < 16){
     alert(`Your BMI is: ${bmi}.\nIt is considered as: Underweight (Severe Thinness).`);
-}
+};
 if (bmi >= 16 && bmi <= 16.9){
     alert(`Your BMI is: ${bmi}.\nIt is considered as: Underweight (Moderate thinness).`);
-}
+};
 if (bmi >= 17 && bmi <= 18.4){
     alert(`Your BMI is: ${bmi}.\nIt is considered as: Underweight (Mild thinness).`);
-}
-if (bmi < 25) {
-    console.log("Normal");
-} else {
-    console.log("Overweight");
-}
+};
+if (bmi >= 18.5 && bmi <= 24.9){
+    alert(`Your BMI is: ${bmi}.\nIt is considered as: Normal`);
+};
+if (bmi >= 25.0 && bmi <= 29.9){
+    alert(`Your BMI is: ${bmi}.\nIt is considered as: Overweight`);
+};
