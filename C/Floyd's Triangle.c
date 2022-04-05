5797f15ed9627eb4c6fce15281ceccb1978ac1e6
E-rick23 <erickmarques63@gmail.com>
2022-04-05 20:57:16 -0300

    Updated Floyd's Triangle.c

--- C/Floyd's Triangle.c
+++ C/Floyd's Triangle.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
+    int n, counter1, counter2, rows;
+
+    counter1 = counter2 = 1;
+    scanf("%d", &n);
+    
+    if (n <= 0)
+        printf("You typed %d, which isn't a positive number or equals zero", n);
+    else
+        while (counter2 <= n)
+        {
+            rows = 1;
+            while (rows <= counter2)
+            {
+                printf(" %2d", counter1);
+                counter1++;
+                rows++;
+            }
+            counter2++;
+            printf(" \n");
+        }
     return 0;
 };
