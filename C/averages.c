c3955fda8f78488ea5e672b8c77bed48dde0129f
E-rick23 <67922086+E-rick23@users.noreply.github.com>
2022-04-06 13:02:14 -0300

    Update averages.c

--- C/averages.c
+++ C/averages.c
@@ -3,8 +3,9 @@
 
 int main(){
 
-    float num, sum, mult, division, counter, ahritmetic, harmonic, geometric;
+    float eh, eg, em, num, sum, mult, division, counter, arithmetic, harmonic, geometric;
     mult = 1;
+    eh = eg = em = num = sum = division = counter = arithmetic = harmonic = geometric = 0;
     while (counter < 10){
         scanf("%f", &num);
         sum = sum + num;
@@ -12,12 +13,17 @@ int main(){
         division = division + 1/num;
         counter++;
     }
-    ahritmetic = sum/10;
+    arithmetic = sum/10;
     geometric = pow(mult,(float) 1/10);
     harmonic = 10/division;
+    eh = (harmonic - arithmetic)/arithmetic;
+    eg = (geometric - arithmetic)/arithmetic;
+    em = (eh + eg)/2;
+    em = em*100;
 
-    printf("Média aritmética é %.2f\n", ahritmetic);
+    printf("Média aritmética é %.2f\n", arithmetic);
     printf("Média harmônica é %.2f\n", harmonic);
     printf("Média geométrica é %.2f\n", geometric);
+    printf("Erro médio é %.2f %%", em);
     return 0;
 }
