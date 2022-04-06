185bdbb04f9c03721eaf9bbfa08d3afc2252b0ce
E-rick23 <erickmarques63@gmail.com>
2022-04-06 19:16:01 -0300

    Updated commentaries on First and last digit.c

--- C/First and last digit.c
+++ C/First and last digit.c
@@ -1,14 +1,20 @@
+//This code simply shows the first and last digit of the entered number, and the sum of both.
 #include <stdio.h>
 
 int main(){
+    //Declaring variables...
     int number, first, last;
     number = first = last = 0;
+    //Asking for input.
     scanf("%d", &number);
+    //Operation to get the last number.
     last = number % 10;
+    //Operation to get the first number.
     while ( number >= 10 ){
         number = number/10;
     }
     first = number;
+    //Printing the results...
     printf("The last digit is: %d\n", last);
     printf("The first digit is: %d\n", first);
     number = first+last;
