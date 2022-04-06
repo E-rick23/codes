25c736a987887299002d05e38c99efb803940048
E-rick23 <67922086+E-rick23@users.noreply.github.com>
2022-04-06 12:24:56 -0300

    Added harmonic average

--- C/averages.c
+++ C/averages.c
@@ -2,14 +2,16 @@
 
 int main(){
 
-    float num, sum, mult, counter, ahritmetic, harmonic, geometrico;
+    float num, sum, mult, div, counter, ahritmetic, harmonic, geometric;
     while (counter < 10){
         scanf("%f", num);
         sum = sum + num;
-        mult = mult* num;
+        mult = mult * num;
+        div = div + 1/num;
         counter++;
     }
     ahritmetic = sum/10;
-
+    geometric = pow(mult,(float) 1/10);
+    harmonic = 10/div;
     return 0;
 }
