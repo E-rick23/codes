a8cc218f04c8c09c9a80776a6d24ba305ddd2282
E-rick23 <67922086+E-rick23@users.noreply.github.com>
2022-04-06 11:20:17 -0300

    Added ahritmetic average

--- C/averages.c
+++ C/averages.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main(){
+
+    float num, sum, counter, ahritmetic, harmonic, geometrico;
+    while (counter < 10){
+        scanf("%f", num);
+        sum = sum + num;
+        counter++;
+    }
+    ahritmetic = sum/10;
+    
     return 0;
 }
