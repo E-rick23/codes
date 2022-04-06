1b182fd447fd1a20bfe4246c985089c5b5f06d81
E-rick23 <erickmarques63@gmail.com>
2022-04-05 21:50:20 -0300

    Updated Make an X.C

--- C/Make an X.c
+++ C/Make an X.c
@@ -2,5 +2,22 @@
 
 int main()
 {
+    int c1, c2, n, counter;
+    char character;
+    
+    scanf("%d %c", &n, &character);
+    counter = n-1;
+
+    for(c1 = 1; c1 <= counter; c1++){
+        for(c2 = 1; c2 <= counter; c2++){
+            if (c2==c1 || (c2==counter - c1 + 1)){
+                printf("%c", character);
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    };
     return 0;
 }
