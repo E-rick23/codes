ab594a1d113721e9f3c83360e9969b0dcaaf2785
E-rick23 <67922086+E-rick23@users.noreply.github.com>
2022-04-06 11:33:16 -0300

    Started to add geometric average variables.

--- C/averages.c
+++ C/averages.c
@@ -2,13 +2,14 @@
 
 int main(){
 
-    float num, sum, counter, ahritmetic, harmonic, geometrico;
+    float num, sum, mult, counter, ahritmetic, harmonic, geometrico;
     while (counter < 10){
         scanf("%f", num);
         sum = sum + num;
+        mult = mult* num;
         counter++;
     }
     ahritmetic = sum/10;
-    
+
     return 0;
 }
