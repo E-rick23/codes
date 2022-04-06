bea5814977f9acd4bb79fcf48d423481befd8927
E-rick23 <erickmarques63@gmail.com>
2022-04-06 19:31:52 -0300

    Added commentaries on Make an X.c

--- C/Make an X.c
+++ C/Make an X.c
@@ -1,13 +1,16 @@
+//This program prints an "X" with the amount of lines, and character, the user wishes to be printed.
 #include <stdio.h>
 
 int main()
 {
+    //Declaring variables...
     int c1, c2, n, counter;
     char character;
+    //Adquiring the lines and character.
     printf("Type the number of lines and the character you want to see in the X: ");
     scanf("%d %c", &n, &character);
     counter = n;
-
+    //For loop that draws the X on the screen.
     for(c1 = 1; c1 <= counter; c1++){
         for(c2 = 1; c2 <= counter; c2++){
             if (c2==c1 || (c2==counter - c1 + 1)){
