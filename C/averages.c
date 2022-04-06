05ba397c83e5b1e5ea870433258d06246828c180
E-rick23 <67922086+E-rick23@users.noreply.github.com>
2022-04-06 12:40:19 -0300

    Finished adding ahritmetic, harmonic and geometric averages

--- C/averages.c
+++ C/averages.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
 
-    float num, sum, mult, div, counter, ahritmetic, harmonic, geometric;
+    float num, sum, mult, division, counter, ahritmetic, harmonic, geometric;
+    mult = 1;
     while (counter < 10){
-        scanf("%f", num);
+        scanf("%f", &num);
         sum = sum + num;
         mult = mult * num;
-        div = div + 1/num;
+        division = division + 1/num;
         counter++;
     }
     ahritmetic = sum/10;
     geometric = pow(mult,(float) 1/10);
-    harmonic = 10/div;
+    harmonic = 10/division;
+
+    printf("Média aritmética é %.2f\n", ahritmetic);
+    printf("Média harmônica é %.2f\n", harmonic);
+    printf("Média geométrica é %.2f\n", geometric);
     return 0;
 }
