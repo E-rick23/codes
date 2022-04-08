32a3dbbf7649da2444f086d2c40baf1477c1a273
E-rick23 <erickmarques63@gmail.com>
2022-04-07 22:02:33 -0300

    Updated commentaries on Palindrome.c

--- C/Palindrome.c
+++ C/Palindrome.c
@@ -7,13 +7,20 @@ int main(){
     x = reverse = restm = 0;
     printf("Type a number to find out if it's a palindrome, and if it's even or odd: ");  
     scanf("%d", &x);    
+    //After getting the input, the copyx variable gets the same value as the input (x).
     copyx = x;
+    //After that, this while loop reverses the value in x.
     while(x!=0)    
     {    
      restm=x%10;    
      reverse=reverse*10+restm;    
      x/=10;    
     }    
+    /*
+      After that it compares to check if both numbers are equal to each other. 
+      If so, the program will tell the user the value is a palindrome, else, it will tell the opposite.
+      The program will also tell the user if the value is odd or even, not caring if the value is a palindrome or not.
+    */
     if (copyx == reverse){
         printf("%d is a palindrome ", copyx);
         if ((copyx%2) == 0){
