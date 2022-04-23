211358e51fe979d11d6e6749626275ebbb080935
E-rick23 <erickmarques63@gmail.com>
2022-04-23 16:01:48 -0300

    Added Geometric and Harmonic averages

--- JavaScript/average.js
+++ JavaScript/average.js
@@ -1,9 +1,10 @@
-//This code calculates the average of the amount of numbers the user needs.
+//This code calculates the arithmetic, geometric, and harmonic averages of the amount of numbers the user needs.
 
 //Declaring variables...
 var values, counter, x, total, aaverage, gaverage, haverage, mult, division;
 //Setting variables default value...
-counter = total = average = x = 0;
+counter = total = aaverage = x = gaverage = haverage = division = 0;
+mult = 1;
 //First, the program asks for input, and converts the input to a int number.
 values = parseInt(prompt("Type how many numbers you'll work with: "));
 //After that, this while loop keeps counting until the number of values the user wants to work with is reached.
@@ -11,10 +12,18 @@ while (counter < values){
     x = parseFloat(prompt(`Type the ${counter+1}º value: `));
     //The value given in x is added to the total variable.
     total = total+x;
+    
+    mult = mult * x;
+
+    division = division + 1/x
     //The counter goes up by one, each time a value is entered.
-    counter = counter+1;
+    counter++
 };
 //In the end, the average variable divides the sum of the values entered by the amount of values.
 aaverage = total/values;
+gaverage = Math.pow(mult, 1/values);
+haverage = 10/division;
 //After that, it notifies the user the average of the values.
-alert("The average is:" + aaverage);
+alert("The arithmetic average is: " + aaverage);
+alert("The geometric average is: " + gaverage);
+alert("The harmonic average is: " + haverage);
