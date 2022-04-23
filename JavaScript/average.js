d64edfdec57e1f4e8e68362a72a7db059decfe25
E-rick23 <erickmarques63@gmail.com>
2022-04-23 16:06:17 -0300

    Added commentaries in averages.c

--- JavaScript/average.js
+++ JavaScript/average.js
@@ -12,18 +12,20 @@ while (counter < values){
     x = parseFloat(prompt(`Type the ${counter+1}º value: `));
     //The value given in x is added to the total variable.
     total = total+x;
-    
+    //The value given in x is multiplied in the variable mult, for the geometric average.
     mult = mult * x;
-
+    //The value given in x becomes the divisor of 1 for the harmonic average.
     division = division + 1/x
     //The counter goes up by one, each time a value is entered.
     counter++
 };
-//In the end, the average variable divides the sum of the values entered by the amount of values.
+//In the end, the arithmetic average variable divides the sum of the values entered by the amount of values.
 aaverage = total/values;
+//The geometric average calculates the square root of the multiplied values.
 gaverage = Math.pow(mult, 1/values);
-haverage = 10/division;
-//After that, it notifies the user the average of the values.
+//The harmonic average divides the number of values by the total value of the operation. 
+haverage = values/division;
+//After that, it notifies the user the averages of the values entered.
 alert("The arithmetic average is: " + aaverage);
 alert("The geometric average is: " + gaverage);
 alert("The harmonic average is: " + haverage);
