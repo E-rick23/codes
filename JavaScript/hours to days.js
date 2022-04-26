c9be6f4d250a745a1be79475304af3edf8b3389e
E-rick23 <erickmarques63@gmail.com>
2022-04-25 21:59:25 -0300

    Simplified parseInt commands on hours to days.js

--- JavaScript/hours to days.js
+++ JavaScript/hours to days.js
@@ -4,7 +4,7 @@
 //Declaring variables...
 var hours, days, daysandhours;
 
-hours = parseFloat(prompt("Enter the amount of hours to be turned to days: "));
+hours = parseInt(prompt("Enter the amount of hours to be turned to days: "));
 //Considering 24 hours make a day, the variable days makes a division that assures every 24 hours becomes 1 day.
 days = parseInt(hours/24); 
 //The variable daysandhours gets what's left of the division.
@@ -12,7 +12,7 @@ daysandhours = parseInt(hours%24);
 /*If daysandhours in fact has a value, then, the program will show the user the days, and hours (which are the rest of the division).
   Or else, if the division rest is 0, it will just return the number of days.*/
 if (daysandhours > 0){
-    alert(`${parseInt(hours)} hours in days are: ${days} days ${daysandhours} hours!`);
+    alert(`${(hours)} hours in days are: ${days} days ${daysandhours} hours!`);
 } else {
-    alert(`${parseInt(hours)} hours in days are: ${days}!`);
+    alert(`${(hours)} hours in days are: ${days}!`);
 }
