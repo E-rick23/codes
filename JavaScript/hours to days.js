97d68f26defbb3588d4889fa041509a7810cd094
E-rick23 <erickmarques63@gmail.com>
2022-04-25 22:09:26 -0300

    Updates on hours to days (js and py)

--- JavaScript/hours to days.js
+++ JavaScript/hours to days.js
@@ -12,7 +12,7 @@ daysandhours = parseInt(hours%24);
 /*If daysandhours in fact has a value, then, the program will show the user the days, and hours (which are the rest of the division).
   Or else, if the division rest is 0, it will just return the number of days.*/
 if (daysandhours > 0){
-    alert(`${(hours)} hours in days are: ${days} days ${daysandhours} hours!`);
+    alert(`${(hours)} hours in days are: ${days} days and ${daysandhours} hours!`);
 } else {
-    alert(`${(hours)} hours in days are: ${days}!`);
+    alert(`${(hours)} hours in days are: ${days} days!`);
 }
--- Python/hours to days.py
+++ Python/hours to days.py
@@ -2,9 +2,11 @@ hours = days = daysandhours = 0;
 
 hours = int(input(""));
 
-days = (hours/24);
+days = int((hours/24));
 
-daysandhours = (hours%24);
+daysandhours = int((hours%24));
 
 if (daysandhours > 0):
-    print(str())
+    print(str(hours) + str(" hours in days are: ") + str(days) + str(" days and ") + str(daysandhours) + str(" hours!"));
+else:
+    print(str(hours) + str(" hours in days are: ") + str(days) + str(" days!"));
