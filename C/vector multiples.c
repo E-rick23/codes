afc8df542dc94071504bafda62aaaec00a592eb7
E-rick23 <erickmarques63@gmail.com>
2022-04-27 22:16:17 -0300

    Updated vector multiples.c

--- C/vector multiples.c
+++ C/vector multiples.c
@@ -1,14 +1,43 @@
 #include <stdio.h>
 
+
 int main(){
     //Declaring array...
-    int integers[20], mult[20];
-    //Scanning 20 values...
-    for (int i=0; i<20; i++){
+    int multiplier, size, counter;
+    multiplier = size = 0;
+    printf("To start off, enter the amount of values you'll work with: ");
+    scanf("%d", &size);
+    int integers[size], integersMultiples[size];
+    printf("Next, enter the number you want to check multiples: ");
+    scanf("%d", &multiplier);
+    printf("Good, now type the values you want to check if it's a multiple of %d: ", multiplier);
+
+    //Scanning the amount of values entered in the size variable...
+    
+    for (int i=0; i<size; i++){
         scanf("%d", &integers[i]);
     }
+
+    counter = 0;
+    for (int i=0; i<size; i++){
+        if (integers[i] % multiplier == 0){
+            integersMultiples[counter] = integers[i];
+            counter++;
+        }
+    }
+
+    printf("Multiples of %d: ", multiplier);
+
+    for (int i=0; i<counter; i++){
+        if (i != 0) printf( ", " );
+        printf("%d", integersMultiples[i]);
+    }
+    printf("\n");
     //Printing 20 values...
-    for (int i=0; i<20; i++){
+    printf("The values you entered were: ");
+    
+    for (int i=0; i<size; i++){
+        if (i != 0) printf( ", " );
         printf("%d", integers[i]);
     }
     return 0;
