dc774aa3e9ac9bf6e81b1c4838ab2daa4bed62c4
E-rick23 <erickmarques63@gmail.com>
2022-04-27 21:31:41 -0300

    updated variable names

--- C/vector multiples.c
+++ C/vector multiples.c
@@ -2,14 +2,14 @@
 
 int main(){
     //Declaring array...
-    int inteiros[20];
+    int integers[20], mult[20];
     //Scanning 20 values...
     for (int i=0; i<20; i++){
-        scanf("%d", &inteiros[i]);
+        scanf("%d", &integers[i]);
     }
     //Printing 20 values...
     for (int i=0; i<20; i++){
-        printf("%d", inteiros[i]);
+        printf("%d", integers[i]);
     }
     return 0;
 }
