2bb6ca03764ddaaf46338f642d2b2b42edd59359
E-rick23 <erickmarques63@gmail.com>
2022-05-10 19:11:34 -0300

    Added printf statements on symmetric matrix.c

--- C/symmetric matrix.c
+++ C/symmetric matrix.c
@@ -4,7 +4,11 @@
 int main(){
     int t1, t2;
     bool lever = true;
-    scanf("%d %d", &t1, &t2);
+    printf("Enter the number of rows: ");
+    scanf("%d", &t1);
+    printf("Enter the number of columns: "); 
+    scanf("%d", &t2);
+    printf("Enter the values of the matrix: \n");
     int m[t1][t2];
     for(int i = 0; i < t1; i++)
         for(int j = 0; j < t2; j++)
