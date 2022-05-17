670082afc26c6f4a65aa7930b3a594a46ff486f5
E-rick23 <erickmarques63@gmail.com>
2022-05-17 19:31:52 -0300

    Added commentaries on string splitter.c

--- C/string splitter.c
+++ C/string splitter.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <string.h>
+//This code splits strings using a delimiter character (Both entered by the user).
 int main()
 {
+	//First the user enters a string that will be used by the program, it needs to have 50 characters or less.
 	printf("Enter the string (max 50 characters): ");
 	char str[50];
 	scanf("%[^\n]%*c", str);
-	int init_size = strlen(str);
+	//After that, the user enters a character that will separate the string.
 	char delim[] = " ";
 	printf("Enter the delimiter character: ");
 	scanf("%c", &delim[0]);
 
 	char *ptr = strtok(str, delim);
-
+	//The while loop prints the words separated by the delimiter.
 	while(ptr != NULL)
 	{
 		printf("'%s'\n", ptr);
