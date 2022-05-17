1328779c810dd28c8048aca2d17bac4c01f62c1a
E-rick23 <erickmarques63@gmail.com>
2022-05-17 20:03:08 -0300

    Added sum of main diagonal.js

---
+++ JavaScript/sum of main diagonal.js
@@ -0,0 +1,21 @@
+
+var tam, soma;
+soma = 0;
+tam = parseInt(prompt("Digite o tamanho da matriz:"));
+var m = [];
+for(i = 0; i < tam; i++){
+    m[i] = []
+    for(j = 0; j < tam; j++){
+        m[i][j] = parseInt(prompt(""));
+    }
+}
+
+for(i = 0; i < tam; i++){
+    for(j = 0; j < tam; j++){
+        if (i == j){
+            soma = soma + m[i][j];
+        }
+    }
+}
+alert(`A soma dos valores é: ${soma}`);
+    
