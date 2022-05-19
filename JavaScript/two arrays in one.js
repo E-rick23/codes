25cd0fefc86feb9f79f7a7302dfd5c3da9f75d4f
E-rick23 <erickmarques63@gmail.com>
2022-05-19 12:10:35 -0300

    Updated two arrays in one.js

--- JavaScript/two arrays in one.js
+++ JavaScript/two arrays in one.js
@@ -2,3 +2,12 @@ var firstA = [];
 var firstsize = parseInt(prompt("Type the size of the first array: "));
 var secondA = [];
 var secondsize = parseInt(prompt("Type the size of the second array: "));
+for (i = 0; i < firstsize; i++){
+    firstA[i] = prompt(`Enter the ${i+1}º array value of the first array: `);
+}
+for (i = 0; i < secondsize; i++){
+    secondA[i] = prompt(`Enter the ${i+1}º array value of the second array: `);
+}
+const fusion = firstA.concat(secondA);
+
+alert(`The resulting array is: ${fusion}`);
