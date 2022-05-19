a8f557dd9c9be8324d3b55e7041b3ae08e2fe17e
E-rick23 <erickmarques63@gmail.com>
2022-05-19 12:17:51 -0300

    Updated and added commentaries to sum of main diagonal.js

--- JavaScript/sum of main diagonal.js
+++ JavaScript/sum of main diagonal.js
@@ -1,15 +1,18 @@
 //This code simply makes the sum the main diagonal of a matrix.
+//First we declare the variables we need.
 var tam, soma;
 soma = 0;
-tam = parseInt(prompt("Digite o tamanho da matriz:"));
+//After that, we ask the user the size of the matrix.
+tam = parseInt(prompt("Type the matrix size (Example: if the value is 3, the program will consider it a 3x3 matrix):"));
 var m = [];
+//The following two for loops now adquire the matrix.
 for(i = 0; i < tam; i++){
     m[i] = []
     for(j = 0; j < tam; j++){
         m[i][j] = parseInt(prompt(""));
     }
 }
-
+//Now, those two for loops sums the values of the main diagonal.
 for(i = 0; i < tam; i++){
     for(j = 0; j < tam; j++){
         if (i == j){
@@ -17,5 +20,6 @@ for(i = 0; i < tam; i++){
         }
     }
 }
-alert(`A soma dos valores é: ${soma}`);
+//After that, the sum of the values is printed.
+alert(`The sum of the main diagonal is: ${soma}`);
     
