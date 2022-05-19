faaf598277e217a7a3ca4986b1aabbaa42a44fa0
E-rick23 <erickmarques63@gmail.com>
2022-05-19 12:05:34 -0300

    Updated Find words.c and added two arrays in one.js

--- C/Find words.c
+++ C/Find words.c
@@ -2,7 +2,7 @@
 #include<string.h>
 int main()
 {
-    int i, j, tamstr, tamword, temp, numdevezes = 0, chk;
+    int tamstr, tamword, temp, numdevezes = 0, chk, times = 0, v[100];
     char str[100], word[20];
     printf("Enter the String: ");
     scanf("%[^\n]%*c", str);
@@ -10,19 +10,27 @@ int main()
     scanf("%[^\n]%*c", word);
     tamstr = strlen(str);
     tamword = strlen(word);
-    for(i=0; i<tamstr; i++)
+    for(int i=0; i<tamstr; i++)
     {
         temp = i;
-        for(j=0; j<tamword; j++)
+        for(int j=0; j<tamword; j++)
         {
             if(str[i]==word[j])
+            {   
                 i++;
+            }     
         }
         chk = i-temp;
-        if(chk==tamword)
-            numdevezes++;
-        i = temp;
+        if(chk==tamword){
+            v[times] = i - tamword;
+                times++;
+                 numdevezes++;
+        }
     }
-    printf("The word appeared %d times!", numdevezes);
+    printf("The word appeared %d times! \n", numdevezes);
+    printf("Positions: ");
+    for(int i = 0; i < times; i++){
+        printf("%d ", v[i]);
+    }
     return 0;
 }
---
+++ JavaScript/two arrays in one.js
@@ -0,0 +1,4 @@
+var firstA = [];
+var firstsize = parseInt(prompt("Type the size of the first array: "));
+var secondA = [];
+var secondsize = parseInt(prompt("Type the size of the second array: "));
