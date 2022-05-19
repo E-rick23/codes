ddfe231923becc30fba3e3ebcf2eb8b5204ffb60
E-rick23 <erickmarques63@gmail.com>
2022-05-19 20:04:38 -0300

    Updated find words.c

--- C/Find words.c
+++ C/Find words.c
@@ -1,36 +1,68 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+
+int countOccurrences(char * str, char * toSearch);
+
+int tolower(int argument);
+
 int main()
 {
-    int tamstr, tamword, temp, numdevezes = 0, chk, times = 0, v[100];
-    char str[100], word[20];
-    printf("Enter the String: ");
+    char str[40];
+    char toSearch[40];
+    int count;
+    //Adquiring the string and word.
+    printf("Enter any string: ");
     scanf("%[^\n]%*c", str);
-    printf("Enter a Word: ");
-    scanf("%[^\n]%*c", word);
-    tamstr = strlen(str);
-    tamword = strlen(word);
-    for(int i=0; i<tamstr; i++)
-    {
-        temp = i;
-        for(int j=0; j<tamword; j++)
-        {
-            if(str[i]==word[j])
-            {   
-                i++;
-            }     
-        }
-        chk = i-temp;
-        if(chk==tamword){
-            v[times] = i - tamword;
-                times++;
-                 numdevezes++;
-        }
-    }
-    printf("The word appeared %d times! \n", numdevezes);
-    printf("Positions: ");
-    for(int i = 0; i < times; i++){
-        printf("%d ", v[i]);
-    }
+    printf("Enter word to search occurrences: ");
+    scanf("%[^\n]%*c", toSearch);
+    
+    
+    count = countOccurrences(str, toSearch);
+
     return 0;
+}
+
+int countOccurrences(char * str, char * toSearch)
+{
+    int i, j, found, count, stringLen, searchLen, v[100];
+
+    stringLen = strlen(str);     
+    searchLen = strlen(toSearch); 
+    
+    for(int i=0; i<stringLen; i++){
+       str[i] = tolower(str[i]);
+    }
+    for(int i=0; i<searchLen; i++){
+       toSearch[i] = tolower(toSearch[i]);
+    }
+    count = 0;
+
+    for(i=0; i <= stringLen-searchLen; i++)
+    {
+        
+        found = 1;
+        for(j=0; j<searchLen; j++)
+        {
+            if(str[i + j] != toSearch[j])
+            {
+                found = 0;
+                break;
+            }
+        }
+
+        if(found == 1)
+        {
+            v[count] = i;
+            count++;
+        }
+    }
+    printf("The word appeared: %d times! \n", count);
+    if(count != 0){
+        printf("The positions they're on are: ");
+        for(int i = 0; i < count; i++){
+        printf("%d ", v[i]);
+        }
+    }
+    
+    return count;
 }
