789ae6f0ebaacbccb960c6209a647cdc2909184a
E-rick23 <erickmarques63@gmail.com>
2022-05-26 11:24:31 -0300

    Added commentaries and bugfix on pointers.c

--- C/pointers.c
+++ C/pointers.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
-
+//This code is simply a test with pointers.
 void main(){
+    //Declaring variable
     int a;
+    //Declaring pointer
     int *p;
+    //Giving the variable adress to the pointer
     p = &a;
+    //Scanning value of the a variable
     scanf("%d", &a);
-
+    //Printing the results
     printf("The variable value is: %d", a);
-    pritnf("The pointer value is: %p", *p);
+    printf("The pointer value is: %p", *p);
     printf("The memory address of the variable is: %p", p);
 }
