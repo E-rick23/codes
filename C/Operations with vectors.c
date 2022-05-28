ac5f5029f6e88c67460020d7674fc58d6056ac3d
E-rick23 <erickmarques63@gmail.com>
2022-05-28 12:33:57 -0300

    Added commentaries on Operations with vectors.c

--- C/Operations with vectors.c
+++ C/Operations with vectors.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+//This code waits for the input of three arrays, with three elements each, then, it does the following operation:
+//A[0] = [1st element of 1st array + 1st element of 2nd array - 5 times the 1st element of the 3rd array 
+//A[1] = [2nd element of 1st array + 2nd element of 2nd array - 5 times the 2nd element of the 3rd array
+//A[2] = [3rd element of 1st array + 3rd element of 2nd array - 5 times the 3rd element of the 3rd array  
 int main(){
     int a[3], b[3], c[3], d[3];
     for(int i = 0; i<3; i++){
