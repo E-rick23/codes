2008ce3ab21297cc12e272ccf642ebdf397fa518
E-rick23 <erickmarques63@gmail.com>
2022-05-30 19:36:51 -0300

    Added commentaries on fatorial.c

--- C/fatorial.c
+++ C/fatorial.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
-void fatorar(int* x) {
-    
+void factorial(int* x) {
+    //Declarando variáveis
     int aux, result = 1;
     
     if (*x > 1) {
-        
+        //The aux value is x-1 to calculate the factorial
         aux = *x - 1;
-        
-        fatorar(&aux);
-        
+        //Calling the function again (Using recursion)
+        factorial(&aux);
+        //When the operations are finished, the code will then, place the final value in the result variable.
         result = (*x) * aux;
     }
-    
+    //After the process is complete, the pointer x will receive the result.
     *x = result;
 }
-
 void main(){
     int n;
-   
+    //Entering the number.
     scanf("%d",&n); 
-    
-    fatorar(&n);
-    
+    //Calling the function.
+    factorial(&n);
+    //Printing the results.
     printf("%d", n);
 }
