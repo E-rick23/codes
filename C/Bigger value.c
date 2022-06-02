1cd41cbecbca4a5644a6a8b8047a3a23ebda3b40
E-rick23 <erickmarques63@gmail.com>
2022-06-02 15:44:45 -0300

    updated commentaries on bigger value.c

--- C/Bigger value.c
+++ C/Bigger value.c
@@ -1,22 +1,22 @@
+//This code shows the user the biggest value they entered.
 #include <stdio.h>
- 
 void main()
 {   
-    //Declarando variáveis
+    //Declaring variables
     int array[100], *max, tam;
-    //Recebendo tamanho do vetor
+    //Receiving array size
     scanf("%d", &tam);
-    //Recebendo valores
+    //Receiving values
     for (int i = 0 ; i < tam ; i++ )
         scanf("%d", &array[i]);
     
      max = array;
-     //O ponteiro max recebe o ponteiro da array
     *max = *array;
-    //Loop que adquire o maior valor.
+    //Loop that aquires the biggest value
     for(int i = 1; i < tam; i++){
         if (*(array+i) > *max)
             *max = *(array+i);
     }
+    //Printing the result.
     printf("O maior valor é %d.\n", *max);
 }
