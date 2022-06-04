36fde04d04e811f0570ff8202fc8cfe4de178d92
E-rick23 <erickmarques63@gmail.com>
2022-06-03 21:43:35 -0300

    Added commentaries on party.c

--- C/Party.c
+++ C/Party.c
@@ -3,6 +3,7 @@
 //This code uses a struct to add itens that will be necessary for a party.
 //After all items have been added, it shows the total value, and splits it between the members.
 
+//Declaring a struct
 struct item {
     char nome[100];
     float preco;
@@ -12,6 +13,7 @@ int main(){
     struct item s[10];
     int c = 1, c1 = 1, check = 0, pessoas = 0;
     float total = 0, divisao;
+    //While loop that keeps going until the user inputs 2 after the values.
     while (c==1){
         scanf("%s ,",&*s[c1].nome);
         scanf("%f ,",&s[c1].preco);
@@ -23,6 +25,7 @@ int main(){
         }
         c1++;
     }
+    //Printing the results
     printf("Enter the amount of participants: ");
     scanf("%d", &pessoas);
     divisao = total/pessoas;
