bc9498c4c59e49972e56134c42b0612b8975def1
E-rick23 <erickmarques63@gmail.com>
2022-06-10 19:33:51 -0300

    updated comments on Make an X.c

--- C/Make an X.c
+++ C/Make an X.c
@@ -6,11 +6,11 @@ int main()
     //Declaring variables...
     int c1, c2, n, counter;
     char character;
-    //Adquiring the lines and character.
+    //Acquiring the lines and character.
     printf("Type the number of lines and the character you want to see in the X: ");
     scanf("%d %c", &n, &character);
     counter = n;
-    //For loop that draws the X on the screen.
+    //For loop that prints the X on the screen.
     for(c1 = 1; c1 <= counter; c1++){
         for(c2 = 1; c2 <= counter; c2++){
             if (c2==c1 || (c2==counter - c1 + 1)){
