5f9c53286315571ed5829f4030b06f594f42542a
E-rick23 <erickmarques63@gmail.com>
2022-06-21 18:01:07 -0300

    Added comments on Operations with vectors.c

--- C/Operations with vectors.c
+++ C/Operations with vectors.c
@@ -5,21 +5,26 @@
 //A[2] = [3rd element of 1st array + 3rd element of 2nd array - 5 times the 3rd element of the 3rd array  
 int main(){
     int a[3], b[3], c[3], d[3];
+    //For loop to enter 3 values on the first array.
     for(int i = 0; i<3; i++){
         printf("Enter the [%d]º element of the first array:", i+1);
         scanf("%d", &b[i]);
     }
+    //For loop to enter 3 values on the second array.
     for(int i = 0; i<3; i++){
         printf("Enter the [%d]º element of the second array:", i+1);
         scanf("%d", &c[i]);
     }
+    //For loop to enter 3 values on the third array.
     for(int i = 0; i<3; i++){
         printf("Enter the [%d]º element of the third array:", i+1);
         scanf("%d", &d[i]);
     }
+    //Applying operations.
     a[0] = b[0]+c[0]-(5*d[0]);
     a[1] = b[1]+c[1]-(5*d[1]);
     a[2] = b[2]+c[2]-(5*d[2]);
+    //Printing the results.
     printf("The resulting array is:\n");
     printf("A = [%d,%d,%d]", a[0], a[1], a[2]);
     return 0;
