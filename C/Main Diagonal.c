7119148bfec59b8be82d798826389fbe209ba803
E-rick23 <erickmarques63@gmail.com>
2022-06-21 18:34:47 -0300

    Main Diagonal.c now also shows the secondary diagonal and it's sum.

--- C/Main Diagonal.c
+++ C/Main Diagonal.c
@@ -2,8 +2,8 @@
 //This code simply makes the sum the main diagonal of a matrix.
 void main(){
 	//Declaring variables.
-	int size, total;
-	total = size = 0;
+	int size, principal, secondary;
+	principal = size = secondary = 0;
 	//Scanning the size of the matrix.
 	printf("Hello, this program receives the size of a matrix, it's values, and prints the sum of it's main diagonal.\n\n");
 	printf("On the following step, you'll determine the matrix size.\n\n");
@@ -19,6 +19,7 @@ void main(){
 		    printf("Enter the %dº value of the %dº line: ", j+1, i+1);
 			scanf("%d", &m[i][j]);
 		}
+	//The code now prints the matrix the user entered.
 	printf("\nThis is the matrix you entered: \n");
 	for(int i = 0; i < size; i++)
 		for(int j = 0; j < size; j++){
@@ -26,16 +27,25 @@ void main(){
 			if(j == (size-1))
 				printf("\n");
 		}
-	//After that, the code will print the main diagonal of the matrix and place it's sum on the "total" variable.
-	printf("\nThe values in the main diagonal are: ");
+	//After that, the code will print the main diagonal of the matrix and place it's sum on the "principal" variable.
+	printf("\nThe values in the principal diagonal are: ");
 	for(int i = 0; i < size; i++)
 		for(int j = 0; j < size; j++){
 			if (i == j){
 				printf("%d ", m[i][j]);
-				total = total + m[i][j];
+				principal = principal + m[i][j];
+			}
+		};
+	printf("\nThe values in the secondary diagonal are: ");
+	for(int i = 0; i < size; i++)
+		for(int j = 0; j < size; j++){
+			if ((i + j) == (size - 1)){
+				printf("%d ", m[i][j]);
+				secondary = secondary + m[i][j];
 			}
 				
 		}
 	//Next, for the final step, the program will print the results.
-	printf("\nThe sum of the principal diagonal is: %d", total);
+	printf("\nThe sum of the principal diagonal is: %d", principal);
+	printf("\nThe sum of the secondary diagonal is: %d", secondary);
 }
