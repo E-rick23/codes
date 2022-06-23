44caab6111e2decede33e09ca32e4ab399412425
E-rick23 <erickmarques63@gmail.com>
2022-06-23 17:34:43 -0300

    Added code description on Numeric sequence.c

--- C/Numeric sequence.c
+++ C/Numeric sequence.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/* This code generates a sequence using the following rules:
+  - If the variable "n" is even, it's divided by 2
+  - If the variable "n" is odd, it's multiplied by three, and adds 1.
+  - It keeps going while "n" is different than 1, when "n" is 1, the sequence ends.
+*/
 int sequencing(int n){
     if (n != 1){
         if (n != 2){
