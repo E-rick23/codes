12aae2bf56711440cc98474c7f38880b162bef04
E-rick23 <erickmarques63@gmail.com>
2022-06-23 15:49:08 -0300

    Added code description for LCM and GDC.c

--- C/LCM and GCD.c
+++ C/LCM and GCD.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+//This code calculates the greatest common divisor and least common multiple between two integers using a recursive function.
 //gdc is a recursive function that finds the GDC of the number.
 int gcd(int x, int y)
 {
@@ -20,7 +20,7 @@ void main()
    scanf("%d %d", &n1, &n2);
    //The first call of the gcd function is made.
    maxdc = gcd(n1, n2);
-   //After the recursion is over the results are printed.
+   //After the recursion is over, the results are printed.
    printf("GCD = %d", maxdc);
    printf("\nLCM = %d", (n1 * n2) / maxdc);
 }
