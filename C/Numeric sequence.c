a8aa90961fde8e483e7b793bd7566a2491989268
E-rick23 <erickmarques63@gmail.com>
2022-06-23 17:41:15 -0300

    Updated Numeric sequence.c

--- C/Numeric sequence.c
+++ C/Numeric sequence.c
@@ -3,11 +3,10 @@
   - If the variable "n" is even, it's divided by 2
   - If the variable "n" is odd, it's multiplied by three, and adds 1.
   - It keeps going while "n" is different than 1, when "n" is 1, the sequence ends.
+  The value of "n" is entered by the user.
 */
 int sequencing(int n){
-    if (n != 1){
-        if (n != 2){
-            if((n%2) == 0){
+        if(n%2 == 0){
             printf("%d ", n);
             n = n/2;
         }
@@ -22,10 +21,8 @@ int sequencing(int n){
         }
         if(n != 1)
             sequencing(n);
-    }
-        }
     return 0;
-}
+    }
 void main(){
     int n;
     scanf("%d", &n);
@@ -35,7 +32,7 @@ void main(){
     if(n == 2){
         printf("2 1");
     }
-    if((n != 1) || (n != 2)){
+    if(n > 2){
         sequencing(n);
     }
 }
