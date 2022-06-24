4d9da21c95bd6df1397ada18e70e2960bfd10b86
E-rick23 <erickmarques63@gmail.com>
2022-06-23 21:12:25 -0300

    Added comments on LCM and GCD.js

--- JavaScript/LCM and GCD.js
+++ JavaScript/LCM and GCD.js
@@ -1,13 +1,20 @@
+//This code calculates the greatest common divisor and least common multiple between two integers using a recursive function.
+//gdc is a recursive function that finds the GDC of the number.
 function gcd(x, y) {
+	//The recursion ends if the (y == 0) condition is met.
 	if (y == 0)
 		return x;
 	else
 		return gcd(y, (y%x));
+	//If the condition isn't met, the function calls itself, continuing the process.
 };
 console.log("Enter two integers: ");
+//Adquiring the values.
 var n1 = parseInt(prompt(""));
 var n2 = parseInt(prompt(""));
+//The first call of the gcd function is made.
 maxcd = gcd(n1, n2);
 lcm = (n1*n2)/maxcd
+//After the recursion is over, the results are printed.
 console.log(`GCD = ${maxcd}`);
 console.log(`LCM = ${lcm}`);
