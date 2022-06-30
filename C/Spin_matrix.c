81c3c4e72afe5db26fd2450b196e73034e04f62c
E-rick23 <erickmarques63@gmail.com>
2022-06-30 13:20:31 -0300

    Updated Spin_Matrix.c

--- C/Spin_matrix.c
+++ C/Spin_matrix.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
-//This code spins a matrix 90º degrees
-int main(){
+//This code spins a 3x3 matrix 90º degrees
+void main(){
     int ma[3][3];
+    //Adquiring the matrix values.
     for (int i = 0; i < 3; i++)
         for (int j = 0; j < 3; j++)
-        {
             scanf("%d", &ma[i][j]);
-        }
+    //Spinning the matrix
     for (int i = 0; i < 3; i++){
         for (int j = 2; j > -1; j--)
-        {   
                 printf("%d ", ma[j][i]);
-        }
         printf("\n");
     }
-    return 0;
 }
