b0e4e623333a86aa4d8856a35fc0500227479a05
E-rick23 <erickmarques63@gmail.com>
2022-06-30 22:03:38 -0300

    Added script description on size.sh

--- Shell/size.sh
+++ Shell/size.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+#This script tells the size of a folder or file the user chooses.
+#To run this script on the linux terminal, give it the "execute" permission by typing "sudo chmod u+x" on the terminal.
+#After giving it the execute permission simply use the format "./size.sh /folderorfilename"
+#Remember to use the "cd" command to get on the folder the script is to run it.
 $1 &>/dev/null
 
 TAMANHO={ du -sh $1 | cut -f1
