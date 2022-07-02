1869cb4939881ef73bd77dcfb0f5000bb951f9ac
e-rick23 <erickmarques63@gmail.com>
2022-07-02 14:05:53 -0300

    Added script description for prime.sh

--- Shell/prime.sh
+++ Shell/prime.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#This script tells if the number entered is composite or prime.
+#If the number entered is a composite number, it will also tell it's closest divisor.
+
 $1 &>/dev/null
 for((i=2; i<=$1/2; i++))
 do
