a08dcb9dc5259230fbeafcd945f95fb169b050af
Erick Marques <67922086+E-rick23@users.noreply.github.com>
2022-09-13 14:00:01 -0300

    Removed test code in Binary search.c

--- C/Binary search.c
+++ C/Binary search.c
@@ -5,26 +5,22 @@ void main(){
     for(int i = 0; i < 100; i++){
         vetor[i] = i;
     }
-    /*for(int i = 0; i < 100; i++){
-        printf("%d ", vetor[i]);
-    }*/
     printf("Type the number you want to search: ");
     scanf("%d", &whichnum);
     int middle = (sizeof(vetor)/sizeof(vetor[0]))/2; //Finds out the size of the array and cuts it in half.
     int fullsize = (sizeof(vetor)/sizeof(vetor[0]));
-    //printf("%d", middle);
     if (vetor[middle] == whichnum){
-        printf("Valor encontrado! Indice: %d", middle);
+        printf("Number found! Index: %d", middle);
     } else if (vetor[middle] > whichnum){
         for(int i = 0; i < middle; i++){
             if(vetor[i] == whichnum){
-                printf("Valor encontrado! Indice: %d", i);
+                printf("Number found! Index: %d", i);
             }
         }
     } else {
         for (int i = middle+1; i < fullsize; i++){
             if(vetor[i] == whichnum){
-                printf("Valor encontrado! Indice: %d", i);
+                printf("Number found! Index: %d", i);
             }
         } 
     } 
