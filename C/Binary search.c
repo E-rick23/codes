656c60a70bda267c6b401745905693d09eeb0560
Erick Marques <67922086+E-rick23@users.noreply.github.com>
2022-09-14 10:41:26 -0300

    Fixed some mistakes and set the binary search as a recursive function.

--- C/Binary search.c
+++ C/Binary search.c
@@ -1,43 +1,40 @@
 #include <stdio.h>
-#include <stdbool.h>
-void main(){
-    //Declaring variables.
+ 
+// Binary search function that returns the index of the number entered or -1 in case it isn't on the array.
+int binarySearch(int array[], int l, int r, int x)
+{
+    if (r >= l) {
+        int middle = l + (r - l) / 2;
+        //If the value is on the middle, the function returns the result.
+        if (array[middle] == x)
+            return middle;
+ 
+        // If value is smaller than the middle value, it will search the left subarray.
+        if (array[middle] > x)
+            return binarySearch(array, l, middle - 1, x);
+ 
+        // If the value isn't on the middle and is bigger than the middle value, it will search the value on the right subarray
+        return binarySearch(array, middle + 1, r, x);
+    }
+    //If the element isn't present in the array, the function returns -1.
+    return -1;
+}
+ 
+void main()
+{
     int vetor[100];
     int whichnum;
-    bool foundit = false;
     //Filling an array with values.
     for(int i = 0; i < 100; i++){
         vetor[i] = i;
     }
+    int fullsize = sizeof(vetor) / sizeof(vetor[0]);
     printf("Type the number you want to search: ");
     //Waiting for input.
     scanf("%d", &whichnum);
-    int fullsize = (sizeof(vetor)/sizeof(vetor[0])); //Array size.
-    int middle = fullsize/2; //Half of the array size.
-    //In case the value entered is equal to the value in the middle of the array, the value is found, and the program ends!
-    if (vetor[middle] == whichnum){
-        printf("\nNumber found! Index: %d\n", middle);
-        foundit = true;
-        /*In case the value entered isn't equal to the value in the middle of the array,
-        the program will look at the values that are bigger than the middle value 
-        or smaller than the bigger value, depending on the entered number. */
-    } else if (vetor[middle] > whichnum){
-        for(int i = 0; i < middle; i++){
-            if(vetor[i] == whichnum){
-                printf("\nNumber found! Index: %d\n", i);
-                foundit = true;
-            }
-        }
-    } else {
-        for (int i = middle+1; i < fullsize; i++){
-            if(vetor[i] == whichnum){
-                printf("\nNumber found! Index: %d\n", i);
-                foundit = true;
-            }
-        } 
-    };
-    //After that, if the program isn't able to find the number, the program says the value isn't on the array, and ends.
-    if (foundit == false){
-        printf("\nThe value you entered isn't on this array...\n");
-    } 
+    int result = binarySearch(vetor, 0, fullsize - 1, whichnum);
+    if (result == -1)
+        printf("\nThe value you entered isn't on this array...");
+    else
+        printf("\nThe value you entered was found! Index: %d", result);
 }
