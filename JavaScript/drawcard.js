d4fbd0e1bbe949f1ea900730b08739cd2876d624
Erick Marques <67922086+E-rick23@users.noreply.github.com>
2023-02-04 19:15:13 -0300

    Updated prompt and descriptions

--- JavaScript/drawcard.js
+++ JavaScript/drawcard.js
@@ -1,28 +1,28 @@
 
 function drawCard(arr) {
-    const getanumber = Math.floor(Math.random() * arr.length); //Valor aleatório.
+    const getanumber = Math.floor(Math.random() * arr.length); //Random value within the amount of cards in the deck
     
     const item = arr[getanumber];
     
     arr.splice(getanumber, 1);
     
-    console.log(`Cards restantes: ${arr}`);
+    console.log(`Remaining cards: ${arr}`);
     return item;
 }
-var baralho = ["C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "CJ", "CQ", "CK", "JOKER1", "E1", "E2", "E3", "E4", "E5", "E6", "E7", "E8", "E9", "E10", "EJ", "EQ", "EK", "JOKER2", "O1", "O2", "O3", "O4", "O5", "O6", "O7", "O8", "O9", "O10", "OJ", "OQ", "OK", "JOKER3", "P1", "P2", "P3", "P4", "P5", "P6", "P7", "P8", "P9", "P10", "PJ", "PQ", "PK", "JOKER4"];
+var deck = ["C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "CJ", "CQ", "CK", "JOKER1", "E1", "E2", "E3", "E4", "E5", "E6", "E7", "E8", "E9", "E10", "EJ", "EQ", "EK", "JOKER2", "O1", "O2", "O3", "O4", "O5", "O6", "O7", "O8", "O9", "O10", "OJ", "OQ", "OK", "JOKER3", "P1", "P2", "P3", "P4", "P5", "P6", "P7", "P8", "P9", "P10", "PJ", "PQ", "PK", "JOKER4"];
 
-var temcarta = baralho.length; //Tamanho do baralho.
+var hasCard = deck.length; //Deck size.
 
-var continuar = 2;
+var keepgoing = 2;
 
-while(temcarta > 0){
-    temcarta--
-    continuar = parseInt(prompt("Comprar? (1 - sim, 0 - não): "))
-    if (continuar == 1){
-        console.log(drawCard(baralho));
+while(hasCard > 0){
+    hasCard--
+    keepgoing = parseInt(prompt("Draw? (1 - Yes, 0 - No): "))
+    if (keepgoing == 1){
+        console.log(drawCard(deck));
     } else {
-        console.log(`Cards restantes no final: ${baralho}`)
-        temcarta = 0;
+        console.log(`Remaining cards in the end: ${deck}`)
+        hasCard = 0;
     }
     
 }
