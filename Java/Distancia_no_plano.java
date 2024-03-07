3b6f7aa4ebd00d146950744d224f5c7ad2385d08
e-rick23 <erickmarques63@gmail.com>
2024-03-07 20:04:37 -0300

    added comment that does the same operation but in a while loop

--- Java/Distancia_no_plano.java
+++ Java/Distancia_no_plano.java
@@ -10,5 +10,19 @@ public class Distancia_no_plano {
         y2 = entrada.nextDouble();
         conta = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
         System.out.println(String.format("%.4f", conta));
+
+        /*
+         * same thing but using a while loop.
+         * double conta;
+         * double[] arrayOfDoubles = new double[4];
+         * int c = 0;
+         * while (c < 4) {
+         * arrayOfDoubles[c] = entrada.nextDouble();
+         * c++;
+         * }
+         * conta = Math.sqrt(Math.pow((arrayOfDoubles[1] - arrayOfDoubles[0]), 2)
+         * + Math.pow((arrayOfDoubles[2] - arrayOfDoubles[3]), 2));
+         * System.out.println(String.format("%.4f", conta));
+         */
     }
 }
