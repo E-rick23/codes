5b9f3e86a4f75356e0abd10c448aa8727946ec0f
e-rick23 <erickmarques63@gmail.com>
2025-03-27 18:11:56 -0300

    Added Long_Double_Precision_Test.c

---
+++ C/Long_Double_Precision_Test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    long double pi_ld = 3.141592653589793238462643383279502884L;
+    printf("PI with a long double: %.30Lf\n", pi_ld);
+    return 0;
+}
